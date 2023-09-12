84a2248b30107b770b77465406f60df3507abb3f
nyakiomaina <carolinenyakio21@gmail.com>
2023-09-12 09:33:41 +0300

    add camera progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod map;
 mod player;
 mod map_builder;
+mod camera;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -9,14 +10,19 @@ mod prelude {
     pub use crate::map::*;
     pub use crate::player::*;
     pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
 use prelude::*;
+
+pub const DISPLAY_WIDTH:i32 = SCREEN_WIDTH / 2;
+pub const DISPLAY_HEIGHT:i32 = SCREEN_HEIGHT / 2;
 
 struct State {
     map:Map,
     player: Player,
     map_builder: MapBuilder,
+    camera: Camera,
 }
 
 impl State {
@@ -28,16 +34,14 @@ impl State {
     //         ),
     //     }
     // }
-    fn new()-> Self {
+    fn new() -> Self {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
 
-        let map = map_builder.map.clone();
-        let player_start = map_builder.player_start.clone();
-
         Self {
-            map,
-            player: Player::new(player_start),
+            map: map_builder.map.clone(),
+            player:Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
             map_builder,
         }
     }
@@ -56,6 +60,12 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Dragon")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()
         .map_err(|e| e)?;
 
