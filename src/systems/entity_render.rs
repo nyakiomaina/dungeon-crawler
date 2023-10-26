e55650c6ee678d2e03d326a6fd0dca0d60843898
nyakiomaina <carolinenyakio21@gmail.com>
2023-10-27 00:04:36 +0300

    added comment to the new system and added it to the scheduler

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -4,16 +4,21 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Render)]
 pub fn entity_render(esc: &SubWorld, #[resource] camera: &Camera ) {
+    //batch for drawing
     let mut draw_batch = DrawBatch::new();
-    draw_batch.target(1);
+    draw_batch.target(1); // set the drawing layer target
+    // calculate offset based on camera's position
     let offset = Point::new(camera.left_x, camera.top_y);
 
-    <(&Point, &Render)>::query().iter(ecs).for_each(|(pos, render)| {
+
+    //query entity with Point and Render components
+
+    <(&Point, &Render)>::query().iter(esc).for_each(|(pos, render)| {
         draw_batch.set(
             *pos - offset,
             render.color,
             render.glyph
         );
-    })
-    draw_batch.submit(5000).expect("Batch error")
+    });
+    draw_batch.submit(5000).expect("Batch error");
 }
